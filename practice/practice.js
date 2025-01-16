bb4c0a9f28d3d14fb0aeb1169f1178d1d3966e7a
tylermc42 <freighttrain42@gmail.com>
2025-01-15 19:02:52 -0700

    Week 2 Photo Album with hamburger menu

--- practice/practice.js
+++
@@ -1,9 +0,0 @@
-console.log("hello world");
-console.log(typeof "Dave");
-console.log(typeof 42);
-console.log(typeof true);
-
-//Refused to execute script from 
-// 'http://127.0.0.1:5500/week02/week02/scripts/bom.js' 
-// because its MIME type ('text/html') is not executable, 
-// and strict MIME type checking is enabled
--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,11 @@
+// Get the current year dynamically
 const currentYear = new Date().getFullYear();
-document.getElementById('currentyear').textContent = currentYear;
 
-const lastModifiedDate = new Date(document.lastModified).toLocaleDateString();
-document.getElementById('lastModified').textContent = `Last edited: ${lastModifiedDate}`;
+// Output the current year to the footer's first paragraph
+document.getElementById("copyright-year").textContent = currentYear;
+
+// Get the document's last modified date
+const lastModified = document.lastModified;
+
+// Output the last modified date to the footer's second paragraph
+document.getElementById("last-modified").textContent = lastModified;
