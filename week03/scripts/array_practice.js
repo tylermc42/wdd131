3f8a44b75b5b27c66e3897806f003cb17cdf4056
tylermc42 <freighttrain42@gmail.com>
2025-01-16 13:29:03 -0700

    learning activities

---
+++ week03/scripts/array_practice.js
@@ -0,0 +1,34 @@
+// Given the following code snippet:
+
+let firstName = 'Antonia';
+let lastName = 'Francesca';
+
+// Write a function declaration / definition named 
+// fullName that has two parameters named first and 
+// last. The function's purpose is to combine those 
+// two string parameters together to return one 
+// combined string (a 'full' name) with a space 
+// between the first and last variables.
+function fullName(first, last) {
+  return `${first} ${last}`;   
+  // string concatenation first + ' ' + last (not preferred)
+}
+
+// Now use an anonymous function expression to do 
+// the same thing where the function is assigned 
+// to a variable named fullName.
+const fullName = function (first, last) {
+  return `${first} ${last}`;
+}
+
+// Now use an arrow function expression to do the 
+// same thing where the function is assigned to a 
+// variable named fullName.
+const fullName = (firstName, lastName) => `${firstName} ${lastName}`;
+
+// Write an expression that calls the fullName 
+// function declaration and writes the result 
+// to an existing HTML element's text node with 
+// the ID of fullName
+document.querySelector('#fullName').innerHTML = fullName(firstName, 
+    lastName, lastName);
