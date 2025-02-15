d8ebebf800e6ae0fefcbd2e7fae5ae2a3bd6ca8f
tylermc42 <freighttrain42@gmail.com>
2025-02-15 00:09:36 -0700

    form page edit

--- scripts/form.js
+++ scripts/form.js
@@ -36,8 +36,13 @@ const products = [
     });
 });
 
-// LocalStorage counter for reviews
-window.onload = function() {
-    let reviewCount = localStorage.getItem('reviewCount') || 0;
-    localStorage.setItem('reviewCount', ++reviewCount);
-};
+
+// local storage
+//  Initialize display element variable
+const visitsDisplay = document.querySelector(".visits");
+// Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+// increment the number of visits by one.
+numVisits++;
+// store the new visit total into localStorage, key=numVisits-ls
+localStorage.setItem("numVisits-ls", numVisits);
