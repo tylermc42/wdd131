84611b9bfd9e0b96df6a7e4121af3f9408993a48
tylermc42 <freighttrain42@gmail.com>
2025-02-14 17:06:31 -0700

    adding local storage

--- scripts/project.js
+++ scripts/project.js
@@ -48,3 +48,12 @@ document.querySelector('form').addEventListener('submit', function() {
     document.getElementById("Radio-button").checked=false
 });
 
+// local storage
+//  Initialize display element variable
+const visitsDisplay = document.querySelector(".visits");
+// Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+// increment the number of visits by one.
+numVisits++;
+// store the new visit total into localStorage, key=numVisits-ls
+localStorage.setItem("numVisits-ls", numVisits);
